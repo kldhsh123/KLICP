1d4db4c6f0f79c2e61b5e52e0ded59c1e9ec8d9a
kldhsh123 <123392781+kldhsh123@users.noreply.github.com>
2024-07-25 07:33:28 +0800

    隐私删除

--- db_connection.php
+++ db_connection.php
@@ -1,21 +1,8 @@
 <?php
-// 允许访问的域名列表
-$allowed_domains = array('icp.kldhsh.top');
-
-// 获取当前请求的域名
-$request_domain = $_SERVER['HTTP_HOST'];
-
-// 检查当前请求的域名是否在允许列表中
-if (!in_array($request_domain, $allowed_domains)) {
-    header('HTTP/1.0 403 Forbidden');
-    die('');
-}
-
-// 这里是你的数据库连接代码
 $servername = "localhost"; // 数据库服务器的地址
-$username = "icp_kldhsh_top"; // 你的数据库用户名
-$password = "kldhsh123/Aa123412345./icp.kldhsh.top"; // 你的数据库密码
-$dbname = "icp_kldhsh_top"; // 你想要连接的数据库名
+$username = "text"; // 你的数据库用户名
+$password = "text"; // 你的数据库密码
+$dbname = "text"; // 你想要连接的数据库名
 
 // 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
